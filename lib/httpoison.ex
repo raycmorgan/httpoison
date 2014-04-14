9eb0b806d1cb0468c340314b118bc16cef438d9b
Ray Morgan <ray@stripe.com>
2014-04-13 21:45:18 -0700

    Implement Access & Enumerable protocols for structs

--- lib/httpoison.ex
+++ lib/httpoison.ex
@@ -98,7 +98,7 @@ end
 
 
 defmodule HTTPoison.Response,
- do: defstruct status_code: nil, body: nil, headers: []
+  do: defstruct status_code: nil, body: nil, headers: []
 
 defmodule HTTPoison.AsyncResponse,
  do: defstruct id: nil
@@ -124,4 +124,53 @@ defmodule HTTPoison do
   defexception HTTPError, message: nil
 
   use HTTPoison.Base
+end
+
+
+# Implement Access and Enum protocol for all HTTPoison structs
+
+defimpl Access, for: [
+  HTTPoison.Response,
+  HTTPoison.AsyncResponse,
+  HTTPoison.AsyncStatus,
+  HTTPoison.AsyncHeaders,
+  HTTPoison.AsyncChunk,
+  HTTPoison.AsyncEnd
+] do
+  def access(map, key) do
+    case :maps.find(key, map) do
+      { :ok, value } -> value
+      :error -> nil
+    end
+  end
+end
+
+defimpl Enumerable, for: [
+  HTTPoison.Response,
+  HTTPoison.AsyncResponse,
+  HTTPoison.AsyncStatus,
+  HTTPoison.AsyncHeaders,
+  HTTPoison.AsyncChunk,
+  HTTPoison.AsyncEnd
+] do
+  def reduce(map, acc, fun) do
+    do_reduce(:maps.to_list(map), acc, fun)
+  end
+
+  defp do_reduce(_,     { :halt, acc }, _fun),   do: { :halted, acc }
+  defp do_reduce(list,  { :suspend, acc }, fun), do: { :suspended, acc, &do_reduce(list, &1, fun) }
+  defp do_reduce([],    { :cont, acc }, _fun),   do: { :done, acc }
+  defp do_reduce([h|t], { :cont, acc }, fun),    do: do_reduce(t, fun.(h, acc), fun)
+
+  def member?(map, { key, value }) do
+    { :ok, match?({ :ok, ^value }, :maps.find(key, map)) }
+  end
+
+  def member?(_map, _other) do
+    { :ok, false }
+  end
+
+  def count(map) do
+    { :ok, map_size(map) }
+  end
 end
